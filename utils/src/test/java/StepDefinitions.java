f748e3a8fb72878df696a5a08ce4973d671ae6d2
lkamari <lkamari@pedago.iut-blagnac.fr>
2021-11-09 17:07:14 +0100

    Changes 15

--- utils/src/test/java/StepDefinitions.java
+++ utils/src/test/java/StepDefinitions.java
@@ -6,13 +6,13 @@ public class StepDefinitions {
 
     private Order o;
 
-    @Given("Romeo who wants to create an Order")
-    public void creating_an_order() {
+    @Given("{word} who wants to create an Order")
+    public void creating_an_order(String who) {
         o = new Order();
-        o.setOwner("Romeo");
+        o.setOwner(who);
     }
 
-    @When("Juliet is declared as recipient")
+    @When("{word} is declared as recipient")
     public void declaring_recipient(){
         o.setRecipient("Juliet");
     }
@@ -21,5 +21,23 @@ public class StepDefinitions {
     public void check_emptiness() {
         List<Order.Drink> drinks = o.getDrinks();
         assertEquals(0, drinks.size());
+    }
+
+    @When("a(nother?) {string} is added to the order")
+    public void add_drink_to_the_order(String drinkName){
+        o.getDrinks().add(new Order.Drink(drinkName));
+    }
+
+    @Then("the order contains {int} drink(s?)")
+    public void check_order_size(int size) {
+        assertEquals(size, o.getDrinks().size());
+    }
+
+    @Then("the order contains {int} {string}")
+    public void check_order_contents(int size, String drink) {
+        long count = o.getDrinks().stream()
+            .filter(d -> d.getName().equals(drink))
+            .count();
+        assertEquals(size,count);
     }
 }
